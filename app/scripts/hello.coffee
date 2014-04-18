e0ca165a142a3df6e786d304a08af65e9c192c31
holmesal <siralonsoholmes@gmail.com>
2014-04-17 22:12:27 -0700

    added @2x versions of images and a switch to replace img src tags with @2x versions on retina displays

--- app/scripts/hello.coffee
+++ app/scripts/hello.coffee
@@ -1,1 +1,13 @@
-console.log "'Allo from CoffeeScript!"
+console.log 'Handcrafted by Alonso Holmes, Ethan Sherr, and Matthew Kulp'
+# On launch, track the page load
+mixpanel.track 'Landing page loaded'
+# Track the beta link click
+mixpanel.track_links '.testflightLink', 'Beta link clicked'
+
+# Replace all the normal images with retina images, if on a retina device
+if window.devicePixelRatio >= 1.2
+	images = document.getElementsByTagName 'img'
+	for image in images
+		attr = image.getAttribute 'data-2x'
+		if attr
+			image.src = attr
--- app/scripts/hello.js
+++ app/scripts/hello.js
@@ -1,4 +1,21 @@
 (function() {
-  console.log("'Allo from CoffeeScript!");
+  var attr, image, images, _i, _len;
+
+  console.log('Handcrafted by Alonso Holmes, Ethan Sherr, and Matthew Kulp');
+
+  mixpanel.track('Landing page loaded');
+
+  mixpanel.track_links('.testflightLink', 'Beta link clicked');
+
+  if (window.devicePixelRatio >= 1.2) {
+    images = document.getElementsByTagName('img');
+    for (_i = 0, _len = images.length; _i < _len; _i++) {
+      image = images[_i];
+      attr = image.getAttribute('data-2x');
+      if (attr) {
+        image.src = attr;
+      }
+    }
+  }
 
 }).call(this);
